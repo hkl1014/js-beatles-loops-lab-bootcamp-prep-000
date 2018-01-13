af1a47f017448591185923cc68a0c43488bf5a15
hkl1014 <linghangking93@hotmail.com>
2018-01-13 03:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+
+function iLoveTheBeatles(number){
+  var array=[];
+  do{
+    array.push("I love the Beatles!");
+    number++;
+  }
+  while(number<15);
+}
