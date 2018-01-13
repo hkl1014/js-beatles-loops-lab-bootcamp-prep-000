74f100696c9a60e8c6913c92c4fa36eda344dd94
hkl1014 <linghangking93@hotmail.com>
2018-01-13 03:30:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay(musicians,instruments){
+  var array;
+  for(let i = musicians.length-1;i>0;i++){
+    array.push("${musicians[musicians.length-i]} plays ${instruments[instruments.length-i]}");
+  }
+}
