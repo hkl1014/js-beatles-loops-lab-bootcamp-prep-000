94ca2af3e3816593900118b10126ccb59c509b40
hkl1014 <linghangking93@hotmail.com>
2018-01-13 03:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   var i = facts.length
   while(i>0){
-    facts[i]+="!!!";
+    facts[facts.length-i]+="!!!";
     i--;
   }
   return facts
