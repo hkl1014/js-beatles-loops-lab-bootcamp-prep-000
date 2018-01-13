2a421d40cd216261806deae238b6cf6bd1114341
hkl1014 <linghangking93@hotmail.com>
2018-01-13 03:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians,instruments){
   for(let i = musicians.length;i>0;i--){
     array.push('${musicians[musicians.length-i]} plays ${instruments[instruments.length-i]}');
   }
+  return array
 }
